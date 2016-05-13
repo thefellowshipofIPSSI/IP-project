dd734126a0b4f009d184d5929a97a4de6f5d8825
Auchalet <alexis.michallet@protonmail.com>
2016-05-13 16:51:06 +0200

    Ajout du FOSUserBundle Ajout de Bootstrap mais non configuré

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Ipssi\IpssiBundle\IpssiBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
