3794ce653c845b1033a21add996c958d5344c4fa
auchalet <alexis.michallet@gmail.com>
2016-06-03 08:55:30 +0000

    Ajout d'Assetic/Bootstrap/jQuery

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Ipssi\IpssiBundle\IpssiBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),            
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
