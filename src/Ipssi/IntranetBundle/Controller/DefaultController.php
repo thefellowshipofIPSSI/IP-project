e8f4420386a1978d61d8fbbc723a6b8cc9d11326
Flo <florian@kameleon-mkp.com>
2017-02-09 13:42:08 +0100

    correctif assets

--- src/Ipssi/IntranetBundle/Controller/DefaultController.php
+++ src/Ipssi/IntranetBundle/Controller/DefaultController.php
@@ -32,6 +32,48 @@ class DefaultController extends Controller
             $em->flush();
         }
 
-        return $this->render('IntranetBundle:Default:index.html.twig');
+
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_RH')
+            || $this->get('security.authorization_checker')->isGranted('ROLE_MANAGER')
+            || $this->get('security.authorization_checker')->isGranted('ROLE_SUPERVISEUR')) {
+
+            $cras = count($this->get('intranet.repository.cra')->findAll());
+            $expenses = count($this->get('intranet.repository.expense')->findAll());
+            $vacations = count($this->get('intranet.repository.vacation')->findAll());
+            $users = count($this->get('user.repository.user')->findAll());
+
+            return $this->render('IntranetBundle:Default:index.html.twig', [
+                'cras' => $cras,
+                'expenses' => $expenses,
+                'vacations' => $vacations,
+                'users' => $users,
+            ]);
+
+        } else if ($this->get('security.authorization_checker')->isGranted('ROLE_COLLABORATEUR')) {
+
+            $user_id = $this->getUser()->getId();
+            $cras = count($this->get('intranet.repository.cra')->findBy(['user' => $user_id]));
+            $expenses = count($this->get('intranet.repository.expense')->findBy(['user' => $user_id]));
+            $vacations = count($this->get('intranet.repository.vacation')->findBy(['user' => $user_id]));
+//            $users = count($this->get('user.repository.user')->findBy(['user_id' => $user_id]));
+
+            return $this->render('IntranetBundle:Default:index.html.twig', [
+                'cras' => $cras,
+                'expenses' => $expenses,
+                'vacations' => $vacations,
+//                'users' => $users,
+            ]);
+
+        } else if ($this->get('security.authorization_checker')->isGranted('ROLE_REDACTEUR')) {
+            $news = count($this->get('intranet.repository.news')->findAll());
+            $pages = count($this->get('intranet.repository.page')->findAll());
+
+            return $this->render('IntranetBundle:Default:index.html.twig', [
+                'news' => $news,
+                'pages' => $pages,
+            ]);
+        } else {
+            return $this->render('IntranetBundle:Default:index.html.twig');
+        }
     }
 }
